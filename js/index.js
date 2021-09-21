35041cd71e990744d588271e1b3546e26eb62f92
VanHocAGC <vovanhoc12@gmail.com>
2021-09-22 00:10:23 +0700

    brand

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.brand__slide').slick({
+        slidesToShow:5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false
+    });
+});
+          
