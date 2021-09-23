047f07964159bbfeff2834f07b4e4802dd28207b
Hằng Nguyễn <hangnguyen>
2021-09-23 21:51:53 +0700

    add

--- js/index.js
+++ js/index.js
@@ -48,3 +48,18 @@ $(document).ready(function() {
         autoplaySpeed: 2000,
     });
 })
+$(document).ready(function() {
+    $('.brand__slide').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
+    $('.feadback__content').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false
+    });
+});
