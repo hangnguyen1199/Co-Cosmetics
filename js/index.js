1bdef03327030d1bc2098b50228b0a48550d2437
Tran Nhi <79617379+nhitrannn@users.noreply.github.com>
2021-09-24 22:56:43 +0700

    nhi

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// slick slider
+$(document).ready(function(){
+    $('.slider_img_item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+  });
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// slick slider
+$(document).ready(function(){
+    $('.slider_img').slick({
+      
+    });
+  });
