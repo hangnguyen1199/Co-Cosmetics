516434f8d1399474b9d89d08d41b5d266d9c864a
Tran Nhi <79617379+nhitrannn@users.noreply.github.com>
2021-10-01 23:27:57 +0700

    done product

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-// slick slider
-$(document).ready(function(){
-    $('.slider_img_item').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-    }); 
-  });
-
-  
 $(document).ready(function() {
     $('#slide').slick({
         lazyLoad: 'ondemand',
@@ -75,4 +64,3 @@ $(document).ready(function() {
         arrows: false
     });
 });
-
